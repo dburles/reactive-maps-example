aa32fdebd0e4afc1cc29907ba7089e5cc4b4c66a
David Burles <dburles@gmail.com>
2015-03-26 18:05:57 +1100

    initial

---
+++ reactive-maps-example.js
@@ -0,0 +1,53 @@
+Markers = new Mongo.Collection('markers');
+
+if (Meteor.isClient) {
+  Template.map.onCreated(function() {
+    GoogleMaps.ready('map', function(map) {
+      google.maps.event.addListener(map.instance, 'click', function(event) {
+        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+      });
+
+      var markers = {};
+
+      Markers.find().observe({
+        added: function (document) {
+          var marker = new google.maps.Marker({
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(document.lat, document.lng),
+            map: map.instance,
+            id: document._id
+          });
+
+          google.maps.event.addListener(marker, 'dragend', function(event) {
+            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+          });
+
+          markers[document._id] = marker;
+        },
+        changed: function (newDocument, oldDocument) {
+          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+        },
+        removed: function (oldDocument) {
+          markers[oldDocument._id].setMap(null);
+          delete markers[oldDocument._id];
+        }
+      });
+    });
+  });
+
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+
+  Template.map.helpers({
+    mapOptions: function() {
+      if (GoogleMaps.loaded()) {
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
+}
